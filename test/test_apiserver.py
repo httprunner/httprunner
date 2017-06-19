b775e8c217e532d89e83576fb66e27985426fcd5
debugtalk <mail@debugtalk.com>
2017-06-19 16:19:17 +0800

    update api_server and its tests

--- test/test_apiserver.py
+++ test/test_apiserver.py
@@ -14,6 +14,7 @@ class TestApiServer(unittest.TestCase):
         gevent.spawn(lambda: self._api_server.serve_forever())
         gevent.sleep(0.01)
         self.host = "http://127.0.0.1:%i" % self._api_server.server_port
+        self.api_client = requests.Session()
 
     def tearDown(self):
         super(TestApiServer, self).tearDown()
@@ -21,74 +22,106 @@ class TestApiServer(unittest.TestCase):
         self._api_server.stop()
 
     def clear_users(self):
-        url = "%s/api/user/clear" % self.host
-        resp = requests.get(url)
-        return resp
+        url = "%s/api/users" % self.host
+        return self.api_client.delete(url)
 
-    def add_user(self, uid, name, password):
-        url = "%s/api/user/add" % self.host
+    def get_users(self):
+        url = "%s/api/users" % self.host
+        return self.api_client.get(url)
+
+    def create_user(self, uid, name, password):
+        url = "%s/api/users/%d" % (self.host, uid)
         data = {
-            'uid': uid,
             'name': name,
             'password': password
         }
-        resp = requests.post(url, json=data)
-        return resp
+        return self.api_client.post(url, json=data)
+
+    def get_user(self, uid):
+        url = "%s/api/users/%d" % (self.host, uid)
+        return self.api_client.get(url)
+
+    def update_user(self, uid, name, password):
+        url = "%s/api/users/%d" % (self.host, uid)
+        data = {
+            'name': name,
+            'password': password
+        }
+        return self.api_client.put(url, json=data)
+
+    def delete_user(self, uid):
+        url = "%s/api/users/%d" % (self.host, uid)
+        return self.api_client.delete(url)
 
     def test_clear_users(self):
         resp = self.clear_users()
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(resp.text, "ok")
+        self.assertEqual(True, resp.json()['success'])
 
-    def test_add_user_not_existed(self):
+    def test_create_user_not_existed(self):
         self.clear_users()
-        resp = self.add_user(1000, 'leo', '123456')
-        self.assertEqual(200, resp.status_code)
-        self.assertEqual(resp.text, "ok")
+        resp = self.create_user(1000, 'user1', '123456')
+        self.assertEqual(201, resp.status_code)
 
-        url = "%s/api/user/1000" % self.host
-        resp = requests.get(url)
-        self.assertEqual(200, resp.status_code)
-        self.assertNotEqual(resp.json(), {})
-
-    def test_add_user_existed(self):
+    def test_create_user_existed(self):
         self.clear_users()
-        resp = self.add_user(1000, 'leo', '123456')
-        self.assertEqual(200, resp.status_code)
-        self.assertEqual(resp.text, "ok")
+        resp = self.create_user(1000, 'user1', '123456')
+        resp = self.create_user(1000, 'user1', '123456')
+        self.assertEqual(500, resp.status_code)
 
-        self.add_user(1000, 'leo2', '123456')
-        url = "%s/api/user/1000" % self.host
-        resp = requests.get(url)
+    def test_get_users_empty(self):
+        self.clear_users()
+        resp = self.get_users()
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(resp.json()['name'], 'leo2')
+        self.assertEqual(resp.json()['count'], 0)
+
+    def test_get_users_not_empty(self):
+        self.clear_users()
+        resp = self.create_user(1000, 'user1', '123456')
+        resp = self.get_users()
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual(resp.json()['count'], 1)
+
+        resp = self.create_user(1001, 'user2', '123456')
+        resp = self.get_users()
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual(resp.json()['count'], 2)
 
     def test_get_user_not_existed(self):
         self.clear_users()
-        url = "%s/api/user/1000" % self.host
-        resp = requests.get(url)
-        self.assertEqual(200, resp.status_code)
-        self.assertEqual(resp.json(), {})
+        resp = self.get_user(1000)
+        self.assertEqual(404, resp.status_code)
+        self.assertEqual(resp.json()['success'], False)
 
     def test_get_user_existed(self):
         self.clear_users()
-        self.add_user(1000, 'leo', '123456')
-        url = "%s/api/user/1000" % self.host
-        resp = requests.get(url)
+        self.create_user(1000, 'user1', '123456')
+        resp = self.get_user(1000)
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(resp.json()['name'], 'leo')
+        self.assertEqual(resp.json()['success'], True)
+
+    def test_update_user_not_existed(self):
+        self.clear_users()
+        resp = self.update_user(1000, 'user1', '123456')
+        self.assertEqual(404, resp.status_code)
+        self.assertEqual(resp.json()['success'], False)
+
+    def test_update_user_existed(self):
+        self.clear_users()
+        self.create_user(1000, 'user1', '123456')
+        resp = self.update_user(1000, 'user2', '123456')
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual(resp.json()['data']['name'], 'user2')
 
     def test_delete_user_not_existed(self):
         self.clear_users()
-        url = "%s/api/user/1000" % self.host
-        resp = requests.delete(url)
-        self.assertEqual(200, resp.status_code)
-        self.assertEqual(resp.text, "not_existed")
+        resp = self.delete_user(1000)
+        self.assertEqual(404, resp.status_code)
+        self.assertEqual(resp.json()['success'], False)
 
     def test_delete_user_existed(self):
         self.clear_users()
-        resp = self.add_user(1000, 'leo', '123456')
-        url = "%s/api/user/1000" % self.host
-        resp = requests.delete(url)
+        self.create_user(1000, 'leo', '123456')
+        resp = self.delete_user(1000)
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(resp.text, "ok")
+        self.assertEqual(resp.json()['success'], True)
