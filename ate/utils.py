45351e3ba75b8f14e42f9bb57d74099ce917581b
debugtalk <mail@debugtalk.com>
2017-06-20 19:43:44 +0800

    utils: testcases loader

---
+++ ate/utils.py
@@ -0,0 +1,22 @@
+import json
+import yaml
+import os.path
+from ate.exception import ParamsError
+
+def load_yaml_file(yaml_file):
+    with open(yaml_file, 'r+') as stream:
+        return yaml.load(stream)
+
+def load_json_file(json_file):
+    with open(json_file) as data_file:
+        return json.load(data_file)
+
+def load_testcases(testcase_file_path):
+    file_suffix = os.path.splitext(testcase_file_path)[1]
+    if file_suffix == '.json':
+        return load_json_file(testcase_file_path)
+    elif file_suffix in ['.yaml', '.yml']:
+        return load_yaml_file(testcase_file_path)
+    else:
+        # '' or other suffix
+        raise ParamsError("Bad testcase file name!")
