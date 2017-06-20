898e0f27facd3bd6af30b3bcf97736bae2a377dd
debugtalk <mail@debugtalk.com>
2017-06-20 20:04:10 +0800

    create ApiServerUnittest for public use

--- test/test_apiserver.py
+++ test/test_apiserver.py
@@ -1,25 +1,7 @@
-import multiprocessing
-import time
-import unittest
 import requests
-from . import api_server
+from .base import ApiServerUnittest
 
-class TestApiServer(unittest.TestCase):
-    """
-    Test case class that sets up an HTTP server which can be used within the tests
-    """
-    @classmethod
-    def setUpClass(cls):
-        cls.api_server_process = multiprocessing.Process(
-            target=api_server.app.run
-        )
-        cls.api_server_process.start()
-        time.sleep(0.1)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.api_server_process.terminate()
-
+class TestApiServer(ApiServerUnittest):
     def setUp(self):
         super(TestApiServer, self).setUp()
         self.host = "http://127.0.0.1:5000"
