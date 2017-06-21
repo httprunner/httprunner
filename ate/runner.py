badc1fa6edeff442c81d49f5125a028fe3de21ff
Leo Lee <mail@debugtalk.com>
2017-06-21 18:43:15 +0800

    TestRunner: run testcase suite

--- ate/runner.py
+++ ate/runner.py
@@ -19,3 +19,9 @@ class TestRunner(object):
         diff_content = utils.diff_response(resp_obj, testcase['response'])
         success = False if diff_content else True
         return success, diff_content
+
+    def run_testcase_suite(self, testcase_sets):
+        return [
+            self.run_single_testcase(testcase)
+            for testcase in testcase_sets
+        ]
