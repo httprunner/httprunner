1965a7ddcea9a9d6501786b89197e99585ecf8c1
debugtalk <mail@debugtalk.com>
2017-06-21 18:26:02 +0800

    TestRunner: run single testcase

---
+++ ate/runner.py
@@ -0,0 +1,21 @@
+import requests
+from ate import utils, exception
+
+class TestRunner(object):
+
+    def __init__(self):
+        self.client = requests.Session()
+
+    def run_single_testcase(self, testcase):
+        req_kwargs = testcase['request']
+
+        try:
+            url = req_kwargs.pop('url')
+            method = req_kwargs.pop('method')
+        except KeyError:
+            raise exception.ParamsError("Params Error")
+
+        resp_obj = self.client.request(url=url, method=method, **req_kwargs)
+        diff_content = utils.diff_response(resp_obj, testcase['response'])
+        success = False if diff_content else True
+        return success, diff_content
