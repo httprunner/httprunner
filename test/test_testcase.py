4896c4b934826cf4dd5e54081209492864263697
Leo Lee <mail@debugtalk.com>
2017-06-25 23:04:14 +0800

    TestcaseParser: parse testcase_template, replace all variables with bind value.

---
+++ test/test_testcase.py
@@ -0,0 +1,81 @@
+import unittest
+
+from ate.testcase import TestcaseParser
+from ate import exception
+
+
+class TestcaseParserUnittest(unittest.TestCase):
+
+    def setUp(self):
+        self.variables_binds = {
+            "uid": "1000",
+            "random": "A2dEx",
+            "authorization": "a83de0ff8d2e896dbd8efb81ba14e17d",
+            "json": {
+                "name": "user1",
+                "password": "123456"
+            },
+            "expected_status": 201,
+            "expected_success": True
+        }
+        self.testcase_parser = TestcaseParser(self.variables_binds)
+
+    def test_parse_testcase_template(self):
+        testcase = {
+            "request": {
+                "url": "http://127.0.0.1:5000/api/users/${uid}",
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "authorization": "${authorization}",
+                    "random": "${random}"
+                },
+                "body": "${json}"
+            },
+            "response": {
+                "status_code": "${expected_status}",
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": {
+                    "success": "${expected_success}",
+                    "msg": "user created successfully."
+                }
+            }
+        }
+        parsed_testcase = self.testcase_parser.parse(testcase)
+
+        self.assertEqual(
+            parsed_testcase["request"]["url"],
+            "http://127.0.0.1:5000/api/users/%s" % self.variables_binds["uid"]
+        )
+        self.assertEqual(
+            parsed_testcase["request"]["headers"]["authorization"],
+            self.variables_binds["authorization"]
+        )
+        self.assertEqual(
+            parsed_testcase["request"]["headers"]["random"],
+            self.variables_binds["random"]
+        )
+        self.assertEqual(
+            parsed_testcase["request"]["body"],
+            self.variables_binds["json"]
+        )
+        self.assertEqual(
+            parsed_testcase["response"]["status_code"],
+            self.variables_binds["expected_status"]
+        )
+        self.assertEqual(
+            parsed_testcase["response"]["body"]["success"],
+            self.variables_binds["expected_success"]
+        )
+
+    def test_parse_testcase_template_miss_bind_variable(self):
+        testcase = {
+            "request": {
+                "url": "http://127.0.0.1:5000/api/users/${uid}",
+                "method": "${method}"
+            }
+        }
+        with self.assertRaises(exception.ParamsError):
+            self.testcase_parser.parse(testcase)
