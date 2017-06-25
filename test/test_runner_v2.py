6a578e78516973511707c01d4da77eced73c86cb
httprunner <httprunner>
2017-06-25 20:29:59 +0800

    change demo testcase file name

--- test/test_runner_v2.py
+++ test/test_runner_v2.py
@@ -17,26 +17,30 @@ class TestRunnerV2(ApiServerUnittest):
         return requests.delete(url, headers=self.prepare_headers())
 
     def test_run_single_testcase_yaml(self):
-        testcase_file_path = os.path.join(os.getcwd(), 'test/data/demo_auth.yml')
+        testcase_file_path = os.path.join(
+            os.getcwd(), 'test/data/simple_demo_auth_hardcode.yml')
         testcases = utils.load_testcases(testcase_file_path)
         success, _ = self.test_runner.run_single_testcase(testcases[0])
         self.assertTrue(success)
 
     def test_run_single_testcase_json(self):
-        testcase_file_path = os.path.join(os.getcwd(), 'test/data/demo_auth.json')
+        testcase_file_path = os.path.join(
+            os.getcwd(), 'test/data/simple_demo_auth_hardcode.json')
         testcases = utils.load_testcases(testcase_file_path)
         success, _ = self.test_runner.run_single_testcase(testcases[0])
         self.assertTrue(success)
 
     def test_run_testcase_auth_suite_yaml(self):
-        testcase_file_path = os.path.join(os.getcwd(), 'test/data/demo_auth.yml')
+        testcase_file_path = os.path.join(
+            os.getcwd(), 'test/data/simple_demo_auth_hardcode.yml')
         testcases = utils.load_testcases(testcase_file_path)
         result = self.test_runner.run_testcase_suite(testcases)
         self.assertEqual(len(result), 2)
         self.assertEqual(result, [(True, {}), (True, {})])
 
     def test_run_testcase_auth_suite_json(self):
-        testcase_file_path = os.path.join(os.getcwd(), 'test/data/demo_auth.json')
+        testcase_file_path = os.path.join(
+            os.getcwd(), 'test/data/simple_demo_auth_hardcode.json')
         testcases = utils.load_testcases(testcase_file_path)
         result = self.test_runner.run_testcase_suite(testcases)
         self.assertEqual(len(result), 2)
