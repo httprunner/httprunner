378147a83be71e0b1ba1bde01be93ab763e5ada9
httprunner <httprunner>
2017-06-26 15:47:45 +0800

    TestRunner: testsets can be configured in public config

--- test/test_runner_v2.py
+++ test/test_runner_v2.py
@@ -36,17 +36,17 @@ class TestRunnerV2(ApiServerUnittest):
         testcase_file_path = os.path.join(
             os.getcwd(), 'test/data/simple_demo_auth_hardcode.yml')
         testcases = utils.load_testcases(testcase_file_path)
-        result = self.test_runner.run_testsets(testcases)
-        self.assertEqual(len(result), 2)
-        self.assertEqual(result, [(True, {}), (True, {})])
+        results = self.test_runner.run_testsets(testcases)
+        self.assertEqual(len(results), 2)
+        self.assertEqual(results, [(True, {}), (True, {})])
 
     def test_run_testcase_auth_suite_json(self):
         testcase_file_path = os.path.join(
             os.getcwd(), 'test/data/simple_demo_auth_hardcode.json')
         testcases = utils.load_testcases(testcase_file_path)
-        result = self.test_runner.run_testsets(testcases)
-        self.assertEqual(len(result), 2)
-        self.assertEqual(result, [(True, {}), (True, {})])
+        results = self.test_runner.run_testsets(testcases)
+        self.assertEqual(len(results), 2)
+        self.assertEqual(results, [(True, {}), (True, {})])
 
     def test_run_testcase_template_yaml(self):
         testcase_file_path = os.path.join(
@@ -55,4 +55,11 @@ class TestRunnerV2(ApiServerUnittest):
         success, _ = self.test_runner.run_test(testcases[0]["test"])
         self.assertTrue(success)
         success, _ = self.test_runner.run_test(testcases[1]["test"])
-        self.assertFalse(success)
+        self.assertTrue(success)
+
+    def test_run_testcase_template_sets_yaml(self):
+        testcase_file_path = os.path.join(
+            os.getcwd(), 'test/data/demo_template_sets.yml')
+        testcases = utils.load_testcases(testcase_file_path)
+        results = self.test_runner.run_testsets(testcases)
+        self.assertEqual(results, [(True, {}), (True, {})])
