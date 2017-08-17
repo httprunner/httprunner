78ac4049c1a57dde09778fb6c3c9bf7a5e28322a
httprunner <httprunner>
2017-08-17 17:15:41 +0800

    bugfix: open file with utf-8 encoding

--- ate/utils.py
+++ ate/utils.py
@@ -32,11 +32,11 @@ def get_sign(*args):
     return sign
 
 def load_yaml_file(yaml_file):
-    with open(yaml_file, 'r+') as stream:
+    with open(yaml_file, 'r+', encoding='utf-8') as stream:
         return yaml.load(stream)
 
 def load_json_file(json_file):
-    with open(json_file) as data_file:
+    with open(json_file, encoding='utf-8') as data_file:
         return json.load(data_file)
 
 def load_testcases(testcase_file_path):
