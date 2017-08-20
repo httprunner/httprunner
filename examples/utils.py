09dc36d69da4dc9900582258dfc078c46026cc43
httprunner <httprunner>
2017-08-21 00:14:38 +0800

    add quickstart docs

---
+++ examples/utils.py
@@ -0,0 +1,22 @@
+import hashlib
+import hmac
+import random
+import string
+
+SECRET_KEY = "DebugTalk"
+
+def gen_random_string(str_len):
+    random_char_list = []
+    for _ in range(str_len):
+        random_char = random.choice(string.ascii_letters + string.digits)
+        random_char_list.append(random_char)
+
+    random_string = ''.join(random_char_list)
+    return random_string
+
+def get_sign(*args):
+    content = ''.join(args).encode('ascii')
+    sign_key = SECRET_KEY.encode('ascii')
+    sign = hmac.new(sign_key, content, hashlib.sha1).hexdigest()
+    return sign
+
