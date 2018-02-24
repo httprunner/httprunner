273a01ae2348521a65c55f8fdd5022aa99ea3195
httprunner <httprunner>
2018-02-24 13:47:22 +0800

    print error info when missing debugging type

--- main-debug.py
+++ main-debug.py
@@ -1,6 +1,7 @@
 import sys
 
 from httprunner.cli import main_hrun, main_locust
+from httprunner.logger import color_print
 
 cmd = sys.argv.pop(1)
 
@@ -8,3 +9,11 @@ if cmd in ["hrun", "httprunner", "ate"]:
     main_hrun()
 elif cmd in ["locust", "locusts"]:
     main_locust()
+else:
+    color_print("Miss debugging type.", "RED")
+    example = "\n".join([
+        "e.g.",
+        "python main-debug.py hrun /path/to/testset_file",
+        "python main-debug.py locusts -f /path/to/testset_file"
+    ])
+    color_print(example, "yellow")
