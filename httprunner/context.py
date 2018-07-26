a8d8b26914ed27f7f6256a301eef4999aac89656
debugtalk <mail@debugtalk.com>
2018-07-26 15:08:04 +0800

    deprecate import_module_items

--- httprunner/context.py
+++ httprunner/context.py
@@ -51,11 +51,6 @@ class Context(object):
         function_binds = config_dict.get('function_binds', {})
         self.bind_functions(function_binds, level)
 
-        # import_module_functions will be deprecated soon
-        module_items = config_dict.get('import_module_items', []) \
-            or config_dict.get('import_module_functions', [])
-        self.import_module_items(module_items, level)
-
         variables = config_dict.get('variables') \
             or config_dict.get('variable_binds', OrderedDict())
         self.bind_variables(variables, level)
