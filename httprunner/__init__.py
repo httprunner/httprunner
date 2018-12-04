a7f92f2a8692d142036c7e0b4dbdf6175a3b71b4
httprunner <httprunner>
2018-12-04 14:46:30 +0800

    fix duplicate monkey patch

--- httprunner/__init__.py
+++ httprunner/__init__.py
@@ -2,7 +2,12 @@
 
 try:
     # monkey patch at beginning to avoid RecursionError when running locust.
-    from gevent import monkey; monkey.patch_all()
+    from gevent import monkey
+    if not monkey.is_module_patched('socket'):
+        print("========== monkey patch all ==========")
+        monkey.patch_all()
+    else:
+        print("========== monkey patched ==========")
 except ImportError:
     pass
 
