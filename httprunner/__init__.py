cb8fc091c423dcd3092be6e1212598e458b24367
debugtalk <mail@debugtalk.com>
2019-07-18 16:01:28 +0800

    fix: get project version and description.
    this will be improved after poetry fixed issue https://github.com/sdispater/poetry/issues/144

--- httprunner/__init__.py
+++ httprunner/__init__.py
@@ -1,9 +1,4 @@
-import configparser
-import json
-
-cf_parser = configparser.ConfigParser(allow_no_value=True)
-cf_parser.read("pyproject.toml")
-__version__ = json.loads(cf_parser["tool.poetry"]["version"])
-__description__ = json.loads(cf_parser["tool.poetry"]["description"])
+__version__ = "2.2.4"
+__description__ = "One-stop solution for HTTP(S) testing."
 
 __all__ = ["__version__", "__description__"]
