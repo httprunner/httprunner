32bff3d088a4af5ccdda21e38af94e6017635449
debugtalk <mail@debugtalk.com>
2019-10-26 21:19:12 +0800

    fix: Non-ASCII character when no encoding declared

--- httprunner/response.py
+++ httprunner/response.py
@@ -25,9 +25,9 @@ class ResponseObject(object):
             if key == "json":
                 value = self.resp_obj.json()
             elif key == "cookies":
-                value =  self.resp_obj.cookies.get_dict()
+                value = self.resp_obj.cookies.get_dict()
             else:
-                value =  getattr(self.resp_obj, key)
+                value = getattr(self.resp_obj, key)
 
             self.__dict__[key] = value
             return value
@@ -53,7 +53,7 @@ class ResponseObject(object):
                     }
                 ]
             },
-            "message": "操作成功"
+            "message": "success"
         }
 
         :param field:  Jsonpath expression, e.g. 1)$.code   2) $..items.*.id
