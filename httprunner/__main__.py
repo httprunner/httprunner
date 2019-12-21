a7e203610c83b159ff971125c51877bbd5181bc8
debugtalk <mail@debugtalk.com>
2019-12-21 17:08:46 +0800

    fix: init sentry_sdk before main

--- httprunner/__main__.py
+++ httprunner/__main__.py
@@ -2,10 +2,9 @@ import sys
 
 import sentry_sdk
 
-from httprunner.cli import main
-
 sentry_sdk.init("https://cc6dd86fbe9f4e7fbd95248cfcff114d@sentry.io/1862849")
 
 
 if __name__ == "__main__":
+    from httprunner.cli import main
     sys.exit(main())
