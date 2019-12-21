dc6145af31897c57e6c29029019164631ab26889
debugtalk <mail@debugtalk.com>
2019-12-21 17:21:00 +0800

    fix: ensure initializing sentry_sdk on startup

--- httprunner/__main__.py
+++ httprunner/__main__.py
@@ -1,10 +1,6 @@
 import sys
-
-import sentry_sdk
-
-sentry_sdk.init("https://cc6dd86fbe9f4e7fbd95248cfcff114d@sentry.io/1862849")
+from httprunner.cli import main
 
 
 if __name__ == "__main__":
-    from httprunner.cli import main
     sys.exit(main())
