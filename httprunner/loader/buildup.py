6d5943c0f3922f022cb74a049e9f985b6ad39a00
debugtalk <mail@debugtalk.com>
2019-12-31 19:09:35 +0800

    refactor: rename to JsonSchemaChecker

--- httprunner/loader/buildup.py
+++ httprunner/loader/buildup.py
@@ -2,7 +2,7 @@ import importlib
 import os
 
 from httprunner import exceptions, logger, utils
-from httprunner.loader.check import JsonSchemaCommonChecker, JsonSchemaTestcaseChecker
+from httprunner.loader.check import JsonSchemaChecker
 from httprunner.loader.load import load_module_functions, load_file, load_dot_env_file, \
     load_folder_files
 from httprunner.loader.locate import init_project_working_directory, get_project_working_directory
@@ -225,7 +225,7 @@ def load_testcase_v2(raw_testcase):
             }
 
     """
-    JsonSchemaTestcaseChecker.validate_testcase_v2_format(raw_testcase)
+    JsonSchemaChecker.validate_testcase_v2_format(raw_testcase)
     raw_teststeps = raw_testcase.pop("teststeps")
     raw_testcase["teststeps"] = [
         load_teststep(teststep)
@@ -281,7 +281,7 @@ def load_testsuite(raw_testsuite):
             }
 
     """
-    JsonSchemaCommonChecker.validate_testsuite_format(raw_testsuite)
+    JsonSchemaChecker.validate_testsuite_format(raw_testsuite)
     raw_testcases = raw_testsuite.pop("testcases")
     raw_testsuite["testcases"] = {}
 
@@ -359,7 +359,7 @@ def load_test_file(path):
 
         elif "request" in raw_content:
             # file_type: api
-            JsonSchemaCommonChecker.validate_api_format(raw_content)
+            JsonSchemaChecker.validate_api_format(raw_content)
             loaded_content = raw_content
             loaded_content["path"] = path
             loaded_content["type"] = "api"
