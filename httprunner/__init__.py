d0126eef9cd777d497665763ac76e986c54d7217
debugtalk <mail@debugtalk.com>
2020-01-02 21:30:34 +0800

    fix #819: maximum recursion error in locusts, caused by sentry sdk

--- httprunner/__init__.py
+++ httprunner/__init__.py
@@ -2,15 +2,3 @@ __version__ = "2.5.2"
 __description__ = "One-stop solution for HTTP(S) testing."
 
 __all__ = ["__version__", "__description__"]
-
-import uuid
-
-import sentry_sdk
-
-sentry_sdk.init(
-    dsn="https://cc6dd86fbe9f4e7fbd95248cfcff114d@sentry.io/1862849",
-    release="httprunner@{}".format(__version__)
-)
-
-with sentry_sdk.configure_scope() as scope:
-    scope.set_user({"id": uuid.getnode()})
