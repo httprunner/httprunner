70943c058d5d5d50ea191ef83311daba13d836ab
debugtalk <mail@debugtalk.com>
2020-04-22 23:26:09 +0800

    refactor: adjust summary data structure

--- httprunner/v3/api_test.py
+++ httprunner/v3/api_test.py
@@ -12,6 +12,5 @@ class TestHttpRunner(unittest.TestCase):
         summary = self.runner.run_path("examples/postman_echo/request_methods/")
         self.assertTrue(summary.success)
         self.assertEqual(summary.details[0].name, "request methods testcase with variables")
-        self.assertEqual(summary.details[0].record.name, "request methods testcase with variables")
-        self.assertEqual(summary.stat.testcases["total"], 1)
+        self.assertGreater(summary.stat.total, 1)
         # self.assertEqual(summary.stat.teststeps["total"], 2)
