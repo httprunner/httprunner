4228e6c2b0bfe73dfe7a486863fcc745903f92d3
debugtalk <mail@debugtalk.com>
2020-05-17 12:15:51 +0800

    change: update scaffold for v3

--- httprunner/ext/scaffold/scaffold_test.py
+++ httprunner/ext/scaffold/scaffold_test.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 import unittest
 
 from httprunner.ext.scaffold import create_scaffold
@@ -9,10 +10,16 @@ class TestUtils(unittest.TestCase):
     def test_create_scaffold(self):
         project_name = "projectABC"
         create_scaffold(project_name)
-        self.assertTrue(os.path.isdir(os.path.join(project_name, "api")))
+        self.assertTrue(os.path.isdir(os.path.join(project_name, "har")))
         self.assertTrue(os.path.isdir(os.path.join(project_name, "testcases")))
-        self.assertTrue(os.path.isdir(os.path.join(project_name, "testsuites")))
         self.assertTrue(os.path.isdir(os.path.join(project_name, "reports")))
         self.assertTrue(os.path.isfile(os.path.join(project_name, "debugtalk.py")))
         self.assertTrue(os.path.isfile(os.path.join(project_name, ".env")))
-        shutil.rmtree(project_name)
+
+        # run demo testcases
+        try:
+            subprocess.check_call(["hrun", project_name])
+        except subprocess.SubprocessError:
+            raise
+        finally:
+            shutil.rmtree(project_name)
