780e5b3416c404dd6ee1b8c8838e57845de8ccd5
debugtalk <mail@debugtalk.com>
2020-05-27 21:56:39 +0800

    fix: compatibility with v2, body.lst.0.name

--- httprunner/compat_test.py
+++ httprunner/compat_test.py
@@ -5,18 +5,46 @@ from httprunner import compat
 
 
 class TestCompat(unittest.TestCase):
-
     def test_convert_jmespath(self):
 
+        self.assertEqual(compat.convert_jmespath("content.abc"), "body.abc")
+        self.assertEqual(compat.convert_jmespath("json.abc"), "body.abc")
         self.assertEqual(
-            compat.convert_jmespath("content.abc"),
-            "body.abc"
+            compat.convert_jmespath("headers.Content-Type"), 'headers."Content-Type"'
         )
         self.assertEqual(
-            compat.convert_jmespath("json.abc"),
-            "body.abc"
+            compat.convert_jmespath("body.data.buildings.0.building_id"),
+            "body.data.buildings[0].building_id",
+        )
+
+    def test_convert_extractors(self):
+        self.assertEqual(
+            compat.convert_extractors(
+                [{"varA": "content.varA"}, {"varB": "json.varB"}]
+            ),
+            {"varA": "body.varA", "varB": "body.varB"},
         )
         self.assertEqual(
-            compat.convert_jmespath("headers.Content-Type"),
-            'headers."Content-Type"'
+            compat.convert_extractors([{"varA": "content.0.varA"}]),
+            {"varA": "body[0].varA"},
+        )
+        self.assertEqual(
+            compat.convert_extractors({"varA": "content.0.varA"}),
+            {"varA": "body[0].varA"},
+        )
+
+    def test_convert_validators(self):
+        self.assertEqual(
+            compat.convert_validators(
+                [{"check": "content.abc", "assert": "eq", "expect": 201}]
+            ),
+            [{"check": "body.abc", "assert": "eq", "expect": 201}],
+        )
+        self.assertEqual(
+            compat.convert_validators([{"eq": ["content.abc", 201]}]),
+            [{"eq": ["body.abc", 201]}],
+        )
+        self.assertEqual(
+            compat.convert_validators([{"eq": ["content.0.name", 201]}]),
+            [{"eq": ["body[0].name", 201]}],
         )
