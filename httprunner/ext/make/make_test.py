082cb12e7f77a5e2030d86dc4dcb7946a455681e
debugtalk <mail@debugtalk.com>
2020-05-27 20:58:40 +0800

    fix: ensure pytest files are in python module, generate __init__.py on demand

--- httprunner/ext/make/make_test.py
+++ httprunner/ext/make/make_test.py
@@ -31,13 +31,13 @@ class TestLoader(unittest.TestCase):
             self.assertIn(
                 """
 from examples.postman_echo.request_methods.request_with_functions_test import (
-    TestCaseRequestWithFunctions,
+    TestCaseRequestWithFunctions as RequestWithFunctions,
 )
 """,
                 content,
             )
             self.assertIn(
-                '"testcase": TestCaseRequestWithFunctions,', content,
+                '"testcase": RequestWithFunctions,', content,
             )
 
     def test_make_testcase_folder(self):
@@ -61,28 +61,26 @@ from examples.postman_echo.request_methods.request_with_functions_test import (
             "/path/to 2/mubu_login_test.py",
         )
         self.assertEqual(
-            convert_testcase_path("/path/to 2/mubu.login.yml")[1], "TestCaseMubuLogin"
+            convert_testcase_path("/path/to 2/mubu.login.yml")[1], "MubuLogin"
         )
         self.assertEqual(
             convert_testcase_path("mubu login.yml")[0], "mubu_login_test.py"
         )
         self.assertEqual(
-            convert_testcase_path("/path/to 2/mubu login.yml")[1], "TestCaseMubuLogin"
+            convert_testcase_path("/path/to 2/mubu login.yml")[1], "MubuLogin"
         )
         self.assertEqual(
             convert_testcase_path("/path/to 2/mubu-login.yml")[0],
             "/path/to 2/mubu_login_test.py",
         )
         self.assertEqual(
-            convert_testcase_path("/path/to 2/mubu-login.yml")[1], "TestCaseMubuLogin"
+            convert_testcase_path("/path/to 2/mubu-login.yml")[1], "MubuLogin"
         )
         self.assertEqual(
             convert_testcase_path("/path/to 2/幕布login.yml")[0],
             "/path/to 2/幕布login_test.py",
         )
-        self.assertEqual(
-            convert_testcase_path("/path/to/幕布login.yml")[1], "TestCase幕布Login"
-        )
+        self.assertEqual(convert_testcase_path("/path/to/幕布login.yml")[1], "幕布Login")
 
     def test_make_testsuite(self):
         path = ["examples/postman_echo/request_methods/demo_testsuite.yml"]
