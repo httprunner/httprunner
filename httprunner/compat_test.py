803881f5b9c466a855a2ab520bd60af556ee179b
debugtalk <mail@debugtalk.com>
2020-05-28 11:14:24 +0800

    test: add unittest for ensure_testcase_v3

--- httprunner/compat_test.py
+++ httprunner/compat_test.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 
 from httprunner import compat
@@ -47,4 +46,91 @@ class TestCompat(unittest.TestCase):
         self.assertEqual(
             compat.convert_validators([{"eq": ["content.0.name", 201]}]),
             [{"eq": ["body[0].name", 201]}],
+        )
+
+    def test_ensure_testcase_v3_api(self):
+        api_content = {
+            "name": "get with params",
+            "request": {
+                "method": "GET",
+                "url": "/get",
+                "params": {"foo1": "bar1", "foo2": "bar2"},
+                "headers": {"User-Agent": "HttpRunner/3.0"},
+            },
+            "extract": [{"varA": "content.varA"}, {"user_agent": "headers.User-Agent"}],
+            "validate": [{"eq": ["content.varB", 200]}, {"lt": ["json.0.varC", 0]}],
+        }
+        self.assertEqual(
+            compat.ensure_testcase_v3_api(api_content),
+            {
+                "config": {"name": "get with params"},
+                "teststeps": [
+                    {
+                        "name": "get with params",
+                        "request": {
+                            "method": "GET",
+                            "url": "/get",
+                            "params": {"foo1": "bar1", "foo2": "bar2"},
+                            "headers": {"User-Agent": "HttpRunner/3.0"},
+                        },
+                        "extract": {
+                            "varA": "body.varA",
+                            "user_agent": 'headers."User-Agent"',
+                        },
+                        "validate": [
+                            {"eq": ["body.varB", 200]},
+                            {"lt": ["body[0].varC", 0]},
+                        ],
+                    }
+                ],
+            },
+        )
+
+    def test_ensure_testcase_v3(self):
+        testcase_content = {
+            "config": {"name": "xxx", "base_url": "https://httpbin.org"},
+            "teststeps": [
+                {
+                    "name": "get with params",
+                    "request": {
+                        "method": "GET",
+                        "url": "/get",
+                        "params": {"foo1": "bar1", "foo2": "bar2"},
+                        "headers": {"User-Agent": "HttpRunner/3.0"},
+                    },
+                    "extract": [
+                        {"varA": "content.varA"},
+                        {"user_agent": "headers.User-Agent"},
+                    ],
+                    "validate": [
+                        {"eq": ["content.varB", 200]},
+                        {"lt": ["json.0.varC", 0]},
+                    ],
+                }
+            ],
+        }
+        self.assertEqual(
+            compat.ensure_testcase_v3(testcase_content),
+            {
+                "config": {"name": "xxx", "base_url": "https://httpbin.org"},
+                "teststeps": [
+                    {
+                        "name": "get with params",
+                        "request": {
+                            "method": "GET",
+                            "url": "/get",
+                            "params": {"foo1": "bar1", "foo2": "bar2"},
+                            "headers": {"User-Agent": "HttpRunner/3.0"},
+                        },
+                        "extract": {
+                            "varA": "body.varA",
+                            "user_agent": 'headers."User-Agent"',
+                        },
+                        "validate": [
+                            {"eq": ["body.varB", 200]},
+                            {"lt": ["body[0].varC", 0]},
+                        ],
+                    }
+                ],
+            },
         )
