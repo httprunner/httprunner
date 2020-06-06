0215908700e51a00a3925466aeecf1a53f177cb6
debugtalk <mail@debugtalk.com>
2020-06-06 10:05:57 +0800

    change: format copied debugtalk.py

---
+++ examples/postman_echo/debugtalk.py
@@ -0,0 +1,13 @@
+from httprunner import __version__
+
+
+def get_httprunner_version():
+    return __version__
+
+
+def sum_two(m, n):
+    return m + n
+
+
+def get_variables():
+    return {"foo1": "session_bar1"}
