33112642f0554fd0a0f9d0112efc89f859b22ed3
debugtalk <mail@debugtalk.com>
2021-09-28 15:48:53 +0800

    feat: support set debug for boomer

--- boomer.go
+++ boomer.go
@@ -13,23 +13,32 @@ func Run(testcases ...*TestCase) {
 }
 
 func NewBoomer() *Boomer {
-	return &Boomer{}
+	return &Boomer{
+		debug: false,
+	}
 }
 
 type Boomer struct {
+	debug bool
+}
+
+func (b *Boomer) SetDebug(debug bool) *Boomer {
+	b.debug = debug
+	return b
 }
 
 func (b *Boomer) Run(testcases ...*TestCase) {
 	var taskSlice []*boomer.Task
 	for _, testcase := range testcases {
-		task := convertBoomerTask(testcase)
+		task := b.convertBoomerTask(testcase)
 		taskSlice = append(taskSlice, task)
 	}
 	boomer.Run(taskSlice...)
 }
 
-func convertBoomerTask(testcase *TestCase) *boomer.Task {
+func (b *Boomer) convertBoomerTask(testcase *TestCase) *boomer.Task {
 	runner := NewRunner()
+	runner = runner.SetDebug(b.debug)
 	return &boomer.Task{
 		Name:   testcase.Config.Name,
 		Weight: testcase.Config.Weight,
