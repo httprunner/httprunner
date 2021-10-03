0a96da2f418fc19a71b2611443469a8bffb91bae
debugtalk <mail@debugtalk.com>
2021-10-03 22:56:22 +0800

    feat: post json with functions

--- examples/function_test.go
+++ examples/function_test.go
@@ -12,22 +12,31 @@ func TestCaseCallFunction(t *testing.T) {
 			Name:    "run request with functions",
 			BaseURL: "https://postman-echo.com",
 			Verify:  false,
+			Variables: map[string]interface{}{
+				"n": 5,
+				"a": 12.3,
+				"b": 3.45,
+			},
 		},
 		TestSteps: []httpboomer.IStep{
 			httpboomer.Step("get with params").
-				WithVariables(map[string]interface{}{
-					"n": 5,
-					"a": 12.3,
-					"b": 3.45,
-				}).
 				GET("/get").
 				WithParams(map[string]interface{}{"foo1": "${gen_random_string($n)}", "foo2": "${max($a, $b)}"}).
 				WithHeaders(map[string]string{"User-Agent": "HttpBoomer"}).
 				Extract().
 				WithJmesPath("body.args.foo1", "varFoo1").
 				Validate().
+				AssertEqual("status_code", 200, "check status code").
 				AssertLengthEqual("body.args.foo1", 5, "check args foo1").
 				AssertEqual("body.args.foo2", "12.3", "check args foo2"), // notice: request params value will be converted to string
+			httpboomer.Step("post json data with functions").
+				POST("/post").
+				WithHeaders(map[string]string{"User-Agent": "HttpBoomer"}).
+				WithJSON(map[string]interface{}{"foo1": "${gen_random_string($n)}", "foo2": "${max($a, $b)}"}).
+				Validate().
+				AssertEqual("status_code", 200, "check status code").
+				AssertLengthEqual("body.json.foo1", 5, "check args foo1").
+				AssertEqual("body.json.foo2", 12.3, "check args foo2"),
 		},
 	}
 
