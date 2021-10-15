46b1304fb6e1d525bf85b84cb00b7a95582be998
debugtalk <mail@debugtalk.com>
2021-10-15 10:06:42 +0800

    feat: har2case scaffold

--- har2case/core_test.go
+++ har2case/core_test.go
@@ -4,12 +4,14 @@ import (
 	"log"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var harPath string
 
 func TestMain(m *testing.M) {
-	harPath = "demo.har"
+	harPath = "../examples/demo.har"
 
 	// run all tests
 	code := m.Run()
@@ -21,4 +23,71 @@ func TestMain(m *testing.M) {
 func TestGenJSON(t *testing.T) {
 	jsonPath, err := NewHAR(harPath).GenJSON()
 	log.Printf("jsonPath: %v, err: %v", jsonPath, err)
+	if !assert.NoError(t, err) {
+		t.Fail()
+	}
+	if !assert.NotEmpty(t, jsonPath) {
+		t.Fail()
+	}
+}
+
+func TestLoadHAR(t *testing.T) {
+	har := NewHAR(harPath)
+	h, err := har.load()
+	if !assert.NoError(t, err) {
+		t.Fail()
+	}
+	if !assert.Equal(t, "GET", h.Log.Entries[0].Request.Method) {
+		t.Fail()
+	}
+	if !assert.Equal(t, "POST", h.Log.Entries[1].Request.Method) {
+		t.Fail()
+	}
+}
+
+func TestMakeTestCase(t *testing.T) {
+	har := NewHAR(harPath)
+	tCase, err := har.makeTestCase()
+	if !assert.NoError(t, err) {
+		t.Fail()
+	}
+
+	// make request method
+	if !assert.EqualValues(t, "GET", tCase.TestSteps[0].Request.Method) {
+		t.Fail()
+	}
+	if !assert.EqualValues(t, "POST", tCase.TestSteps[1].Request.Method) {
+		t.Fail()
+	}
+
+	// make request url
+	if !assert.Equal(t, "https://postman-echo.com/get", tCase.TestSteps[0].Request.URL) {
+		t.Fail()
+	}
+	if !assert.Equal(t, "https://postman-echo.com/post", tCase.TestSteps[1].Request.URL) {
+		t.Fail()
+	}
+
+	// make request params
+	if !assert.Equal(t, "Kie7p", tCase.TestSteps[0].Request.Params["foo1"]) {
+		t.Fail()
+	}
+
+	// make request cookies
+	if !assert.NotEmpty(t, tCase.TestSteps[1].Request.Cookies["sails.sid"]) {
+		t.Fail()
+	}
+
+	// make request headers
+	if !assert.Equal(t, "HttpBoomer", tCase.TestSteps[0].Request.Headers["User-Agent"]) {
+		t.Fail()
+	}
+	if !assert.Equal(t, "postman-echo.com", tCase.TestSteps[0].Request.Headers["Host"]) {
+		t.Fail()
+	}
+
+	// make request data
+	if !assert.Equal(t, map[string]interface{}{"foo1": "Kie7p", "foo2": 12.3}, tCase.TestSteps[1].Request.Data) {
+		t.Fail()
+	}
 }
