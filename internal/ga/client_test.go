f44662066e6a174d8ac9b915170a7b73b92caddb
debugtalk <mail@debugtalk.com>
2021-11-26 16:42:28 +0800

    fix: EventTracking ev field type

--- internal/ga/client_test.go
+++ internal/ga/client_test.go
@@ -8,6 +8,7 @@ func TestSendEvents(t *testing.T) {
 	event := EventTracking{
 		Category: "unittest",
 		Action:   "SendEvents",
+		Value:    "123",
 	}
 	err := gaClient.SendEvent(event)
 	if err != nil {
@@ -20,9 +21,10 @@ func TestStructToUrlValues(t *testing.T) {
 		Category: "unittest",
 		Action:   "convert",
 		Label:    "StructToUrlValues",
+		Value:    "123",
 	}
 	val := structToUrlValues(event)
-	if val.Encode() != "ea=convert&ec=unittest&el=StructToUrlValues" {
+	if val.Encode() != "ea=convert&ec=unittest&el=StructToUrlValues&ev=123" {
 		t.Fail()
 	}
 }
