17657d73a462c70ec547fb59b6ccaee0847b3ba4
debugtalk <mail@debugtalk.com>
2021-12-30 16:40:04 +0800

    fix: skip set ulimit on windows

--- hrp/cmd/boom.go
+++ hrp/cmd/boom.go
@@ -1,11 +1,8 @@
 package cmd
 
 import (
-	"runtime"
-	"syscall"
 	"time"
 
-	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
 	"github.com/httprunner/hrp"
@@ -22,8 +19,8 @@ var boomCmd = &cobra.Command{
   $ hrp boom examples/	# run testcases in specified folder`,
 	Args: cobra.MinimumNArgs(1),
 	PreRun: func(cmd *cobra.Command, args []string) {
-		setUlimit()
-		setLogLevel("WARN") // disable info logs for load testing
+		boomer.SetUlimit(10240) // ulimit -n 10240
+		setLogLevel("WARN")     // disable info logs for load testing
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		var paths []hrp.ITestCase
@@ -70,32 +67,4 @@ func init() {
 	boomCmd.Flags().DurationVar(&cpuProfileDuration, "cpu-profile-duration", 30*time.Second, "CPU profile duration.")
 	boomCmd.Flags().StringVar(&prometheusPushgatewayURL, "prometheus-gateway", "", "Prometheus Pushgateway url.")
 	boomCmd.Flags().BoolVar(&disableConsoleOutput, "disable-console-output", false, "Disable console output.")
-}
-
-// set resource limit
-// ulimit -n 10240
-func setUlimit() {
-	log.Info().Str("runtime.GOOS", runtime.GOOS).Msg("check GOOS")
-	if runtime.GOOS == "windows" {
-		log.Warn().Msg("windows does not support setting ulimit")
-		return
-	}
-	var rLimit syscall.Rlimit
-	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-	if err != nil {
-		log.Error().Err(err).Msg("get ulimit failed")
-		return
-	}
-	log.Info().Uint64("limit", rLimit.Cur).Msg("get current ulimit")
-	if rLimit.Cur >= 10240 {
-		return
-	}
-
-	rLimit.Cur = 10240
-	log.Info().Uint64("limit", rLimit.Cur).Msg("set current ulimit")
-	err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-	if err != nil {
-		log.Error().Err(err).Msg("set ulimit failed")
-		return
-	}
 }
