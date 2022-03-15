ee679a7bc29b4c54dc793498f839498981212f4a
xucong053 <xucong053@outlook.com>
2022-03-15 12:39:41 +0800

    feat: support api layer and global headers for testcase

--- boomer_test.go
+++ boomer_test.go
@@ -25,7 +25,7 @@ func TestBoomerStandaloneRun(t *testing.T) {
 			NewStep("TestCase3").CallRefCase(&TestCase{Config: NewConfig("TestCase3")}),
 		},
 	}
-	testcase2 := &TestCasePath{demoTestCaseJSONPath}
+	testcase2 := &demoTestCaseJSONPath
 
 	b := NewBoomer(2, 1)
 	go b.Run(testcase1, testcase2)
