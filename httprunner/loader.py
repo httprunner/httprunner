c57714bbfef23f0adeb049ccf9323b6894391410
debugtalk <mail@debugtalk.com>
2022-03-22 16:34:07 +0800

    change: load yaml file with FullLoader

--- httprunner/loader.py
+++ httprunner/loader.py
@@ -4,17 +4,14 @@ import json
 import os
 import sys
 import types
-from typing import Tuple, Dict, Union, Text, List, Callable
+from typing import Callable, Dict, List, Text, Tuple, Union
 
 import yaml
 from loguru import logger
 from pydantic import ValidationError
-from yaml.loader import FullLoader
 
-from httprunner import builtin, utils
-from httprunner import exceptions
-from httprunner.models import TestCase, ProjectMeta, TestSuite
-
+from httprunner import builtin, exceptions, utils
+from httprunner.models import ProjectMeta, TestCase, TestSuite
 
 project_meta: Union[ProjectMeta, None] = None
 
@@ -24,7 +21,7 @@ def _load_yaml_file(yaml_file: Text) -> Dict:
     """
     with open(yaml_file, mode="rb") as stream:
         try:
-            yaml_content = yaml.load(stream, loader=FullLoader)
+            yaml_content = yaml.load(stream, Loader=yaml.FullLoader)
         except yaml.YAMLError as ex:
             err_msg = f"YAMLError:\nfile: {yaml_file}\nerror: {ex}"
             logger.error(err_msg)
