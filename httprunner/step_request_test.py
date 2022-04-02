acd85136dd23732734605c020eceff64446fd41b
debugtalk <mail@debugtalk.com>
2022-04-02 10:28:25 +0800

    fix: run referenced testcase in python

---
+++ httprunner/step_request_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from examples.postman_echo.request_methods.request_with_functions_test import TestCaseRequestWithFunctions
+
+
+class TestRunRequest(unittest.TestCase):
+
+    def test_run_request(self):
+        runner = TestCaseRequestWithFunctions().test_start()
+        summary = runner.get_summary()
+        self.assertTrue(summary.success)
+        self.assertEqual(summary.name, "request methods testcase with functions")
+        self.assertEqual(len(summary.step_datas), 3)
+        self.assertEqual(summary.step_datas[0].name, "get with params")
+        self.assertEqual(summary.step_datas[1].name, "post raw text")
+        self.assertEqual(summary.step_datas[2].name, "post form data")
