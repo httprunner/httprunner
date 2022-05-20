f155d6007dd503be92d41c5698237593977505f6
xucong053 <xucong053@outlook.com>
2022-05-20 15:03:37 +0800

    fix: unittest

--- hrp/testcase.go
+++ hrp/testcase.go
@@ -287,8 +287,8 @@ func LoadTestCases(iTestCases ...ITestCase) ([]*TestCase, error) {
 			testCasePath := TestCasePath(path)
 			tc, err := testCasePath.ToTestCase()
 			if err != nil {
-				log.Error().Err(err).Str("path", path).Msg("load testcase failed")
-				return errors.Wrap(err, "load testcase failed")
+				log.Warn().Err(err).Str("path", path).Msg("load testcase failed")
+				return nil
 			}
 			testCases = append(testCases, tc)
 			return nil
