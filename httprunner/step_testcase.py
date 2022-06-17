abfd6bb53a956ede18a589d198f01c3f767c83c7
debugtalk <mail@debugtalk.com>
2022-06-17 23:30:45 +0800

    refactor: rename variables

--- httprunner/step_testcase.py
+++ httprunner/step_testcase.py
@@ -22,7 +22,7 @@ def run_step_testcase(runner: HttpRunner, step: TStep) -> StepResult:
 
     # step.testcase is a referenced testcase, e.g. RequestWithFunctions
     ref_case_runner = step.testcase()
-    ref_case_runner.set_references().with_session(runner.session).with_case_id(
+    ref_case_runner.set_referenced().with_session(runner.session).with_case_id(
         runner.case_id
     ).with_variables(step_variables).with_export(step_export).test_start()
 
