fdc9f382d4a2b4507780433487e1e54f8c3a7b83
debugtalk <mail@debugtalk.com>
2022-10-01 22:40:29 +0800

    feat: run ios xctest

--- hrp/cmd/adb/init.go
+++ hrp/cmd/adb/init.go
@@ -3,9 +3,8 @@ package adb
 import "github.com/spf13/cobra"
 
 var androidRootCmd = &cobra.Command{
-	Use:              "adb",
-	Short:            "simple utils for android device management",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {},
+	Use:   "adb",
+	Short: "simple utils for android device management",
 }
 
 func Init(rootCmd *cobra.Command) {
