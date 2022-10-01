9135f2a6b6ccf47187bd6cb9ba826bd981a557c6
debugtalk <mail@debugtalk.com>
2022-10-01 22:40:29 +0800

    feat: run ios xctest

--- hrp/cmd/ios/init.go
+++ hrp/cmd/ios/init.go
@@ -3,9 +3,8 @@ package ios
 import "github.com/spf13/cobra"
 
 var iosRootCmd = &cobra.Command{
-	Use:              "ios",
-	Short:            "simple utils for ios device management",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {},
+	Use:   "ios",
+	Short: "simple utils for ios device management",
 }
 
 func Init(rootCmd *cobra.Command) {
