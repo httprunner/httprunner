9b1f7d2d07433f7887b64c4d51706947f893dd41
debugtalk <mail@debugtalk.com>
2022-10-11 10:40:44 +0800

    refactor: move httpstat from hrp internal to pkg

--- hrp/step_request.go
+++ hrp/step_request.go
@@ -21,8 +21,8 @@ import (
 	"github.com/rs/zerolog/log"
 
 	"github.com/httprunner/httprunner/v4/hrp/internal/builtin"
-	"github.com/httprunner/httprunner/v4/hrp/internal/httpstat"
 	"github.com/httprunner/httprunner/v4/hrp/internal/json"
+	"github.com/httprunner/httprunner/v4/hrp/pkg/httpstat"
 )
 
 type HTTPMethod string
