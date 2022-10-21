278649b8f395fc71d40bc5e029e3f9ac24afbfa4
debugtalk <mail@debugtalk.com>
2022-10-21 15:17:20 +0800

    change: update exit code

--- hrp/internal/code/code.go
+++ hrp/internal/code/code.go
@@ -4,92 +4,102 @@ import (
 	"github.com/pkg/errors"
 )
 
-// general: [0, 20)
+// general: [0, 2)
 const (
 	Success     = 0
 	GeneralFail = 1
 )
 
-// loader: [20, 40)
+// environment: [2, 10)
+
+// loader: [10, 20)
 var (
-	LoadError     = errors.New("load error")      // 20
-	LoadJSONError = errors.New("load json error") // 21
-	LoadYAMLError = errors.New("load yaml error") // 22
+	LoadError     = errors.New("load error")      // 10
+	LoadJSONError = errors.New("load json error") // 11
+	LoadYAMLError = errors.New("load yaml error") // 12
 )
 
-// parser: [40, 60)
+// parser: [20, 30)
 var (
-	ParseError          = errors.New("parse error")            // 40
-	ParseConfigError    = errors.New("parse config error")     // 41
-	ParseStringError    = errors.New("parse string failed")    // 42
-	ParseVariablesError = errors.New("parse variables failed") // 43
+	ParseError          = errors.New("parse error")            // 20
+	ParseStringError    = errors.New("parse string failed")    // 21
+	ParseVariablesError = errors.New("parse variables failed") // 22
+	ParseConfigError    = errors.New("parse config error")     // 25
 )
 
-// runner: [60, 100)
+// runner: [30, 40)
 
-// ios related: [100, 130)
+// summary: [40, 50)
+
+// ios device related: [50, 60)
 var (
-	IOSDeviceConnectionError = errors.New("ios device connection error")  // 100
-	IOSDeviceHTTPDriverError = errors.New("ios device HTTP driver error") // 101
-	IOSDeviceUSBDriverError  = errors.New("ios device USB driver error")  // 102
-	IOSScreenShotError       = errors.New("ios screenshot error")         // 110
-	IOSCaptureLogError       = errors.New("ios capture log error")        // 111
-
-	MobileUIDriverError     = errors.New("mobile UI driver error")     // 120
-	MobileUIValidationError = errors.New("mobile UI validation error") // 121
+	IOSDeviceConnectionError = errors.New("ios device connection error")  // 50
+	IOSDeviceHTTPDriverError = errors.New("ios device HTTP driver error") // 51
+	IOSDeviceUSBDriverError  = errors.New("ios device USB driver error")  // 52
+	IOSScreenShotError       = errors.New("ios screenshot error")         // 55
+	IOSCaptureLogError       = errors.New("ios capture log error")        // 56
 )
 
-// android related: [130, 160)
+// android device related: [60, 70)
 var (
-	AndroidDeviceConnectionError = errors.New("android device connection error") // 130
-	AndroidDeviceDriverError     = errors.New("android device driver error")     // 131
-	AndroidScreenShotError       = errors.New("android screenshot error")        // 150
-	AndroidCaptureLogError       = errors.New("android capture log error")       // 151
+	AndroidDeviceConnectionError = errors.New("android device connection error") // 60
+	AndroidDeviceDriverError     = errors.New("android device driver error")     // 61
+	AndroidScreenShotError       = errors.New("android screenshot error")        // 65
+	AndroidCaptureLogError       = errors.New("android capture log error")       // 66
 )
 
-// OCR related: [160, 180)
+// UI automation related: [70, 80)
 var (
-	OCREnvMissedError         = errors.New("veDEM OCR env missed error")      // 160
-	OCRRequestError           = errors.New("vedem ocr prepare request error") // 161
-	OCRServiceConnectionError = errors.New("vedem ocr service connect error") // 162
-	OCRResponseError          = errors.New("vedem ocr parse response error")  // 163
-	OCRTextNotFoundError      = errors.New("vedem ocr text not found")        // 164
+	MobileUIDriverError     = errors.New("mobile UI driver error")     // 70
+	MobileUIValidationError = errors.New("mobile UI validation error") // 75
 )
 
-// CV related: [180, 200)
+// OCR related: [80, 90)
+var (
+	OCREnvMissedError         = errors.New("OCR env missed error")      // 80
+	OCRRequestError           = errors.New("OCR prepare request error") // 81
+	OCRServiceConnectionError = errors.New("OCR service connect error") // 82
+	OCRResponseError          = errors.New("OCR parse response error")  // 83
+	OCRTextNotFoundError      = errors.New("OCR text not found")        // 84
+)
 
-// report related: [200, 220)
+// CV related: [90, 100)
 
 var errorsMap = map[error]int{
 	// loader
-	LoadJSONError: 10,
-	LoadYAMLError: 11,
+	LoadError:     10,
+	LoadJSONError: 11,
+	LoadYAMLError: 12,
 
 	// parser
-	ParseError:          40,
-	ParseConfigError:    41,
-	ParseStringError:    42,
-	ParseVariablesError: 43,
+	ParseError:          20,
+	ParseStringError:    21,
+	ParseVariablesError: 22,
+	ParseConfigError:    25,
 
 	// ios related
-	IOSDeviceConnectionError: 100,
-	IOSDeviceHTTPDriverError: 101,
-	IOSDeviceUSBDriverError:  102,
-	IOSScreenShotError:       110,
-	IOSCaptureLogError:       111,
+	IOSDeviceConnectionError: 50,
+	IOSDeviceHTTPDriverError: 51,
+	IOSDeviceUSBDriverError:  52,
+	IOSScreenShotError:       55,
+	IOSCaptureLogError:       56,
 
 	// android related
-	AndroidDeviceConnectionError: 130,
-	AndroidDeviceDriverError:     131,
-	AndroidScreenShotError:       150,
-	AndroidCaptureLogError:       151,
+	AndroidDeviceConnectionError: 60,
+	AndroidDeviceDriverError:     61,
+	AndroidScreenShotError:       65,
+	AndroidCaptureLogError:       66,
+
+	// UI automation related
+	MobileUIDriverError:     70,
+	MobileUIValidationError: 75,
 
 	// OCR related
-	OCREnvMissedError:         160,
-	OCRRequestError:           161,
-	OCRServiceConnectionError: 162,
-	OCRResponseError:          163,
-	OCRTextNotFoundError:      164,
+	OCREnvMissedError:         80,
+	OCRRequestError:           81,
+	OCRServiceConnectionError: 82,
+	OCRResponseError:          83,
+	OCRTextNotFoundError:      84,
 }
 
 func GetErrorCode(err error) int {
