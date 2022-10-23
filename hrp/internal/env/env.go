9311e52e44f362e48a91dfaa6ebf72257bc8a2ab
xucong.053 <xucong053@outlook.com>
2022-10-23 21:03:39 +0800

    change: wda ports are used preferentially from environment variables

--- hrp/internal/env/env.go
+++ hrp/internal/env/env.go
@@ -3,12 +3,14 @@ package env
 import "os"
 
 var (
-	WDA_USB_DRIVER = os.Getenv("WDA_USB_DRIVER")
-	VEDEM_OCR_URL  = os.Getenv("VEDEM_OCR_URL")
-	VEDEM_OCR_AK   = os.Getenv("VEDEM_OCR_AK")
-	VEDEM_OCR_SK   = os.Getenv("VEDEM_OCR_SK")
-	DISABLE_GA     = os.Getenv("DISABLE_GA")
-	DISABLE_SENTRY = os.Getenv("DISABLE_SENTRY")
-	PYPI_INDEX_URL = os.Getenv("PYPI_INDEX_URL")
-	PATH           = os.Getenv("PATH")
+	WDA_USB_DRIVER       = os.Getenv("WDA_USB_DRIVER")
+	WDA_LOCAL_PORT       = os.Getenv("WDA_LOCAL_PORT")
+	WDA_LOCAL_MJPEG_PORT = os.Getenv("WDA_LOCAL_MJPEG_PORT")
+	VEDEM_OCR_URL        = os.Getenv("VEDEM_OCR_URL")
+	VEDEM_OCR_AK         = os.Getenv("VEDEM_OCR_AK")
+	VEDEM_OCR_SK         = os.Getenv("VEDEM_OCR_SK")
+	DISABLE_GA           = os.Getenv("DISABLE_GA")
+	DISABLE_SENTRY       = os.Getenv("DISABLE_SENTRY")
+	PYPI_INDEX_URL       = os.Getenv("PYPI_INDEX_URL")
+	PATH                 = os.Getenv("PATH")
 )
