f7eb196a8e9bb9f57ce88c090a184b85f79422e1
lilong.129 <lilong.129@bytedance.com>
2023-08-24 21:33:11 +0800

    fix: TestSleepStrict

--- hrp/internal/scaffold/examples_test.go
+++ hrp/internal/scaffold/examples_test.go
@@ -9,7 +9,7 @@ func TestGenDemoExamples(t *testing.T) {
 	dir := "../../../examples/demo-with-go-plugin"
 	err := CreateScaffold(dir, Go, "", true)
 	if err != nil {
-		t.Fatal()
+		t.Fatal(err)
 	}
 
 	// FIXME
@@ -17,18 +17,18 @@ func TestGenDemoExamples(t *testing.T) {
 	venv := filepath.Join(dir, ".venv")
 	err = CreateScaffold(dir, Py, venv, true)
 	if err != nil {
-		t.Fatal()
+		t.Fatal(err)
 	}
 
 	dir = "../../../examples/demo-without-plugin"
 	err = CreateScaffold(dir, Ignore, "", true)
 	if err != nil {
-		t.Fatal()
+		t.Fatal(err)
 	}
 
 	dir = "../../../examples/demo-empty-project"
 	err = CreateScaffold(dir, Empty, "", true)
 	if err != nil {
-		t.Fatal()
+		t.Fatal(err)
 	}
 }
