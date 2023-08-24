072d0e259e89cfc035b1ce0fd2992b367e7fad6d
lilong.129 <lilong.129@bytedance.com>
2023-08-24 22:03:06 +0800

    fix: TestSleepStrict

--- hrp/pkg/uixt/action_test.go
+++ hrp/pkg/uixt/action_test.go
@@ -40,7 +40,7 @@ func TestSleepStrict(t *testing.T) {
 	sleepStrict(startTime, 1230)
 	dur := time.Since(startTime).Milliseconds()
 	t.Log(dur)
-	if dur < 1230 || dur > 1240 {
+	if dur < 1230 || dur > 1300 {
 		t.Fatalf("sleepRandom failed, dur: %d", dur)
 	}
 }
