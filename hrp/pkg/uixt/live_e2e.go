b83c2a38534fd6f872db3f474b75032641acf2ff
buyuxiang <buyuxiang@bytedance.com>
2023-11-14 20:10:42 +0800

    remove unused endtoenddelay directory

--- hrp/pkg/uixt/live_e2e.go
+++ hrp/pkg/uixt/live_e2e.go
@@ -1,16 +1,13 @@
 package uixt
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"syscall"
 	"time"
 
-	"github.com/httprunner/funplugin/myexec"
 	"github.com/rs/zerolog/log"
 )
 
@@ -24,36 +21,16 @@ type timeLog struct {
 
 type EndToEndDelay struct {
 	driver    *DriverExt
-	file      *os.File
-	resultDir string
-	UUID      string    `json:"uuid"`
-	AppName   string    `json:"appName"`
-	BundleID  string    `json:"bundleID"`
 	StartTime string    `json:"startTime"`
 	EndTime   string    `json:"endTime"`
 	Interval  int       `json:"interval"` // seconds
 	Duration  int       `json:"duration"` // seconds
 	Timelines []timeLog `json:"timelines"`
-	PerfFile  string    `json:"perf"`
 }
 
 func (dExt *DriverExt) CollectEndToEndDelay(options ...ActionOption) {
 	dataOptions := NewActionOptions(options...)
-	var err error
 	startTime := time.Now()
-	resultDir := filepath.Join("endtoenddelay", startTime.Format("2006-01-02 15:04:05"))
-
-	if err = os.MkdirAll(filepath.Join(resultDir, "screenshot"), 0o755); err != nil {
-		log.Fatal().Err(err).Msg("failed to create result dir")
-	}
-
-	filename := filepath.Join(resultDir, "log.txt")
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o755)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to open file")
-	}
-	// write title
-	f.WriteString("utc_time\tutc_timestamp\tlive_time\tlive_seconds\n")
 
 	if dataOptions.Interval == 0 {
 		dataOptions.Interval = 5
@@ -64,14 +41,10 @@ func (dExt *DriverExt) CollectEndToEndDelay(options ...ActionOption) {
 
 	endToEndDelay := &EndToEndDelay{
 		driver:    dExt,
-		file:      f,
-		resultDir: resultDir,
 		Duration:  int(dataOptions.Timeout),
 		Interval:  int(dataOptions.Interval),
 		StartTime: startTime.Format("2006-01-02 15:04:05"),
 	}
-
-	SntpCheckTime()
 
 	endToEndDelay.Start()
 
@@ -122,8 +95,6 @@ func (ete *EndToEndDelay) getCurrentLiveTime(utcTime time.Time) error {
 		liveTimeNSInt = 0
 	}
 	liveTimeStr := time.Unix(int64(liveTimeSInt), int64(liveTimeNSInt*1000*1000)).Format("2006-01-02 15:04:05")
-	line := fmt.Sprintf("%s\t%d\t%s\t%s\n",
-		utcTimeStr, utcTime.UnixMicro(), liveTimeStr, liveTimeText)
 	log.Info().
 		Str("utcTime", utcTimeStr).
 		Int64("utcTimeInt", utcTime.UnixMilli()).
@@ -131,9 +102,6 @@ func (ete *EndToEndDelay) getCurrentLiveTime(utcTime time.Time) error {
 		Int64("liveTimeInt", int64(liveTimeInt)).
 		Float64("delay", float64(utcTime.UnixMilli()-int64(liveTimeInt))/1000).
 		Msg("log live time")
-	if _, err := ete.file.WriteString(line); err != nil {
-		log.Error().Err(err).Str("line", line).Msg("write timeseries failed")
-	}
 	ete.Timelines = append(ete.Timelines, timeLog{
 		UTCTimeStr:  utcTimeStr,
 		UTCTime:     utcTime.UnixMilli(),
@@ -151,8 +119,10 @@ func (ete *EndToEndDelay) Start() {
 	for {
 		select {
 		case <-timer.C:
+			ete.EndTime = time.Now().Format("2006-01-02 15:04:05")
 			return
 		case <-c:
+			ete.EndTime = time.Now().Format("2006-01-02 15:04:05")
 			return
 		default:
 			utcTime := time.Now()
@@ -162,12 +132,5 @@ func (ete *EndToEndDelay) Start() {
 				time.Sleep(500 * time.Millisecond)
 			}
 		}
-	}
-}
-
-func SntpCheckTime() {
-	err := myexec.RunCommand("sudo", "sntp", "-sS", "time.asia.apple.com")
-	if err != nil {
-		log.Error().Err(err).Msg("failed to synchronize time using sntp")
 	}
 }
