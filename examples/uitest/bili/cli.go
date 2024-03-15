602f09074159ffb747d332f8e3ea1d8de6e8ddba
徐聪 <xucong.053@outlook.com>
2024-03-15 19:04:55 +0800

    fix: add waiting time after pressing back

--- examples/uitest/bili/cli.go
+++ examples/uitest/bili/cli.go
@@ -67,6 +67,7 @@ func launchAppDriver(pkgName string) (driver *uixt.DriverExt, err error) {
 }
 
 func watchVideo(driver *uixt.DriverExt) (err error) {
+	time.Sleep(3 * time.Second)
 	err = driver.SwipeUp()
 	if err != nil {
 		return err
@@ -107,12 +108,13 @@ func watchVideo(driver *uixt.DriverExt) (err error) {
 		return err
 	}
 
+	time.Sleep(1 * time.Second)
+
 	// 返回推荐页
 	err = driver.Driver.PressBack()
 	if err != nil {
 		return err
 	}
-	time.Sleep(1 * time.Second)
 
 	return nil
 }
