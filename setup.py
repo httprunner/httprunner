c48875117eb881fe160b8315a299ef05c7279629
diaodeng <wwwxiongjie@gmail.com>
2024-05-16 11:56:23 +0800

    1.修改setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
@@ -7,7 +8,8 @@ setuptools.setup(
     author="diaodeng",  # 作者
     author_email="",  # 作者邮箱
     description="从原httprunner修改的",  # 模块简介
-    long_description="从原httprunner修改用于支持httprunnermanager：https://gitee.com/zywstart/hrm2/tree/hrm4-newui/",  # 模块详细介绍
+    long_description="从原httprunner修改用于支持httprunnermanager：https://gitee.com/zywstart/hrm2/tree/hrm4-newui/",
+    # 模块详细介绍
     long_description_content_type="text",  # 模块详细介绍格式
     url="https://github.com/diaodeng/httprunner/tree/master-diaodeng",  # 模块github地址
     packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
@@ -15,6 +17,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     # 依赖模块
     install_requires=[
