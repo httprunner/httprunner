b412fa193b2fa198ada2b126536364490d0420ae
lilong.129 <lilong.129@bytedance.com>
2025-03-14 10:55:46 +0800

    refactor: move uixt ext to EvalTools

--- cmd/server.go
+++ cmd/server.go
@@ -1,9 +1,8 @@
 package cmd
 
 import (
+	"github.com/httprunner/httprunner/v5/server"
 	"github.com/spf13/cobra"
-
-	server_ext "github.com/httprunner/httprunner/v5/server/ext"
 )
 
 // serverCmd represents the server command
@@ -13,8 +12,7 @@ var serverCmd = &cobra.Command{
 	Long:  `start hrp server, call httprunner by HTTP`,
 	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return server_ext.NewExtRouter().Run(port)
-		// return server.NewRouter().Run(port)
+		return server.NewRouter().Run(port)
 	},
 }
 
